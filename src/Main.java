0aca2029ed6f74e8efa84b428a01c4d9377c8625
Vikusha <victoriaflint01@gmail.com>
2024-02-04 21:17:08 +0300

    Conditions changed

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,11 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
-        List<Integer> positiveNumbers = new ArrayList<>();
-        for (Integer number : intList) {
-            if (number > 0) {
-                positiveNumbers.add(number);
-            }
-        }
-
         List<Integer> evenNumbers = new ArrayList<>();
-        for (Integer number : positiveNumbers) {
-            if (number % 2 == 0) {
-                evenNumbers.add(number);
-            }
-        }
+        intList.stream()
+                .filter(number -> number > 0 && number % 2 == 0)
+                .forEach(evenNumbers::add);
+
 
         evenNumbers.sort(Comparator.naturalOrder());
 
