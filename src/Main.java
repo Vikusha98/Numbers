b1ea2c25f95af64f947316ace4b9a4316752328b
Vikusha <victoriaflint01@gmail.com>
2024-02-05 12:21:27 +0300

    Conditions united

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
         List<Integer> evenNumbers = new ArrayList<>();
-        intList.stream()
-                .filter(number -> number > 0 && number % 2 == 0)
-                .forEach(evenNumbers::add);
-
+        for (Integer number : intList) {
+            if (number > 0 && number % 2 == 0) {
+                evenNumbers.add(number);
+            }
+        }
 
         evenNumbers.sort(Comparator.naturalOrder());
 
